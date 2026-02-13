787157297404c36709a38ce84b6af69555b07d2c
arshsaini15 <arshdeepp015@gmail.com>
2026-02-13 11:56:18 +0530

    synchronized keyword

---
+++ src/MultiThreading/SyncDemo.java
@@ -0,0 +1,42 @@
+package MultiThreading;
+
+class Counter {
+
+    private int count = 0;
+
+    // synchronized ensures only one thread updates count at a time
+    public synchronized void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class SyncDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Counter counter = new Counter();
+
+        // Task that increments counter 10,000 times
+        Runnable task = () -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+
+        t1.start();
+        t2.start();
+
+        // Wait for both threads to finish
+        t1.join();
+        t2.join();
+
+        System.out.println("Final Count: " + counter.getCount());
+    }
+}
